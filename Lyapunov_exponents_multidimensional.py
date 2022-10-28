2e535366ead45cf713154f986044dd15fc12b886
Daniel <111389053+DanielNotAlt@users.noreply.github.com>
2022-10-27 23:39:57 -0400

    Update Lyapunov_exponents_multidimensional.py

--- Lyapunov_exponents_multidimensional.py
+++ Lyapunov_exponents_multidimensional.py
@@ -1,29 +1,6 @@
 from numpy import *
 from numpy.linalg import *
 
-class henon_map:
-
-    def __init__(_, parameters):
-        # initialize parameters to function
-        _.a, _.b = parameters
-
-    def f(_, values):
-        # return values of mapping given x and y
-        x, y = values
-        result = array([1 - _.a * x ** 2 + y, _.b * x])
-        return result
-
-    def M(_, values, vector):
-        # iterate vectors according to the jacobian
-        # because of jacobian, we don't need y
-        x = values[0] 
-        jacobian = array([[-2*_.a*x, 1],[_.b, 0]])
-        return jacobian @ vector
-
-    def __call__(_, values, vector):
-        value_vector = (_.f(values), _.M(values, vector))
-        return value_vector
-
 def lyapunov(system, initial_values=None, iterations=10000, iter_skip=200):
 
     n = len(initial_values)      # dims phase space
@@ -55,8 +32,31 @@ def natural_logarithm(vector):
 
     return apply_along_axis(lambda x: log(norm(x)), 0, vector)
 
+class henon_map:
+
+    def __init__(_, parameters):
+        # initialize parameters to function
+        _.a, _.b = parameters
+
+    def f(_, values):
+        # return values of mapping given x and y
+        x, y = values
+        result = array([1 - _.a * x ** 2 + y, _.b * x])
+        return result
+
+    def M(_, values, vector):
+        # iterate vectors according to the jacobian
+        # because of jacobian, we don't need y
+        x = values[0] 
+        jacobian = array([[-2*_.a*x, 1],[_.b, 0]])
+        return jacobian @ vector
+
+    def __call__(_, values, vector):
+        value_vector = (_.f(values), _.M(values, vector))
+        return value_vector
+
 parameters = (1.4,0.3) # Henon's original parameters for a chaotic attractor
 initial_conditions=array([0.1,0.3]) # Randomly select initial condition for iterates to plot Henon's map
 
-exp = lyapunov(henon_map(parameters), initial_values=initial_conditions).max()
+exp = lyapunov(henon_map(parameters), initial_values=initial_conditions)
 print(f"The maxmimal Lyapunov exponent is: {exp}")
